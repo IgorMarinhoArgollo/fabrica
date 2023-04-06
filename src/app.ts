73df4167ec24dd2afdf17b7ec20a93824552625a
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2023-04-06 18:29:14 -0300

    feat: applying basic security stuff
    applying helmet, rate-limit, morgan, error middleware on app for basic structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import * as express from 'express';
 import errorMiddleware from './middlewares/errorMiddlewares';
+import limiter from './middlewares/limiter';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
+
 
 const app = express();
+
+app.use(helmet());
 app.use(express.json());
+app.use(limiter);
+app.use(morgan('common'));
+
+app.get('/', (req, res) => res.json({ ok: true }));
 
 app.use(errorMiddleware);
 
